8036af4dacf71287f225292a49e8c6947b42a6ab
lucaszw <lucas.zeer@gmail.com>
2017-12-08 15:14:06 -0500

    update conda-build to v3

--- MoscaServer.js
+++ MoscaServer.js
@@ -69,7 +69,7 @@ class MoscaServer {
     if (!clientName) return;
 
     if (!_.includes(clientName, 'mqttjs') && clientName != 'undefined'){
-      console.log({clientName});
+      console.log('client connected', {clientName});
     }
 
     if (clientPath != undefined){
@@ -86,6 +86,7 @@ class MoscaServer {
     }
     this.sendMessage(`${this.channel}/signal/client-disconnected`,
       {clientName: clientName, clientPath: clientPath});
+    console.log('client disconnected', {clientName});
   }
 
   onPublish(packet, client){
