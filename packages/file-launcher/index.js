0bf18abc9d6b07a54fb738a0836c951c6b5c9d92
lucaszw <lucas.zeer@gmail.com>
2018-01-26 15:05:25 -0500

    fix file-launcher for windows
    Former-commit-id: 57bbc09c562410e96b81a56741a2c33050dff28b

--- packages/file-launcher/index.js
+++ packages/file-launcher/index.js
@@ -9,32 +9,15 @@ let eventObj = _.extend({}, backbone.Events);
 let ready = false;
 
 let tray = null
-app.on('ready', () => {
-  eventObj.trigger('ready'); ready = true;
 
-  let image = nativeImage.createFromPath(path.join(__dirname, 'icon.png'));
-  image = image.resize({width: 20, height: 20});
-  tray = new Tray(image);
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
-  tray.setToolTip('Microdrop')
-  tray.setContextMenu(contextMenu)
-});
-
-app.on('open-file', function(event, filePath){
-  event.preventDefault();
-
+function openFile(filepath) {
   var handler = () => {
     var client  = mqtt.connect('mqtt://localhost:1884');
 
     client.on('connect', function () {
       var topic = 'microdrop/file-launcher/state/last-opened-file';
 
-      fs.readFile(filePath, 'utf8', function (err, data) {
+      fs.readFile(filepath, 'utf8', function (err, data) {
         if (err) throw err;
         client.publish(topic, data, (err) => {
           if (err) throw err;
@@ -46,4 +29,33 @@ app.on('open-file', function(event, filePath){
 
   if (ready == false) eventObj.on('ready', handler.bind(this));
   if (ready == true) handler();
+}
+
+app.on('ready', () => {
+  eventObj.trigger('ready'); ready = true;
+
+  const filepath = _.filter(process.argv, (i) => _.includes(i, ".microdrop"))[0];
+  if (filepath) {
+    // dialog.showMessageBox({message: filepath});
+    openFile(filepath);
+  }
+  else {
+    let image = nativeImage.createFromPath(path.join(__dirname, 'icon.png'));
+    image = image.resize({width: 20, height: 20});
+    tray = new Tray(image);
+    const contextMenu = Menu.buildFromTemplate([
+      {label: 'Item1', type: 'radio'},
+      {label: 'Item2', type: 'radio'},
+      {label: 'Item3', type: 'radio', checked: true},
+      {label: 'Item4', type: 'radio'}
+    ])
+    tray.setToolTip('Microdrop')
+    tray.setContextMenu(contextMenu)
+  }
+});
+
+
+app.on('open-file', function(event, filepath){
+  event.preventDefault();
+  openFile(filepath);
 });
