b9cd6613e6e1e6a3e567ef49c7639de5d84a30b7
lucaszw <lucas.zeer@gmail.com>
2018-01-29 12:32:59 -0500

    kill zombie plugins
    Former-commit-id: 725c7ac1d6c9635282c4cc4f8e81f166e2811917

--- packages/application/index.js
+++ packages/application/index.js
@@ -1,10 +1,78 @@
+const PORT = 1884;
+const APPNAME = 'microdrop';
+
 const {spawn} = require('child_process');
 const path = require('path');
+
+const mqtt = require('mqtt');
 
 const OPTIONS = {stdio: 'inherit', shell: true};
 
 console.log("Launching Electron APP");
-spawn(`electron app.js`, [], OPTIONS);
+const broker = spawn(`electron app.js`, [], {shell: true, stdio: 'inherit', detached: true});
 
 console.log("Launching Models");
-spawn(`node ${path.resolve('models', 'launch-models.js')}`, [], OPTIONS);
+const devicePath = path.resolve(__dirname, 'models/DeviceModel.js');
+const elecPath = path.resolve(__dirname, 'models/ElectrodesModel.js');
+const routesPath = path.resolve(__dirname, 'models/RoutesModel.js');
+
+const c1 = spawn(`node ${devicePath}`, [], OPTIONS);
+const c2 = spawn(`node ${elecPath}`, [], OPTIONS);
+const c3 = spawn(`node ${routesPath}`, [], OPTIONS);
+
+async function publishClose() {
+  console.log("publishing close");
+  await new Promise((resolveTop, rejectTop) => {
+    // console.log("connecting to client", `mqtt://localhost:${PORT}`);
+    var client  = mqtt.connect(`mqtt://localhost:${PORT}`);
+    client.on('connect', async function () {
+
+      // console.log("attempting to close routes");
+      await new Promise((resolve, reject) => {
+        console.log("publishing to routes model:")
+        client.publish(`${APPNAME}/trigger/routes-model/exit`, '{}', (err) => {
+          // console.log("closed routes-model");
+          resolve();
+        });
+      });
+
+      // console.log("attempting to close electrodes");
+      await new Promise((resolve, reject) => {
+        console.log("publishing to electrodes model:");
+        client.publish(`${APPNAME}/trigger/electrodes-model/exit`, '{}', (err) => {
+          // console.log("closed electrodes-model");
+          resolve();
+        });
+      });
+
+      // console.log("attempting to close device");
+      await new Promise((resolve, reject) => {
+        console.log("publishing to device model:");
+        client.publish(`${APPNAME}/trigger/device-model/exit`, '{}', (err) => {
+          // console.log("closed device-model");
+          resolve();
+        });
+      });
+      client.end();
+
+      resolveTop();
+    })
+  }).catch((err) => resolveTop(err));;
+};
+
+process.on('SIGINT', () => {
+  publishClose().then((d) => {
+    console.log("plugins finished closing");
+    c1.kill('SIGINT');
+    c2.kill('SIGINT');
+    c3.kill('SIGINT');
+    console.log("killing broker");
+    spawn("taskkill", ["/pid", broker.pid, '/f', '/t'], {detached: true});
+    process.exit();
+  });
+
+  setTimeout(()=>{
+    console.log("timed out waiting for child processes to close.")
+    process.exit();
+  }, 5000);
+});
