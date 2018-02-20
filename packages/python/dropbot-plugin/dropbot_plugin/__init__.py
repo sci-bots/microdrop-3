8ae00f2d265fa73c142048c3be0dc261f13d06f6
lucaszw <lucas.zeer@gmail.com>
2018-02-20 12:46:41 -0500

    add schema example for voltage

--- packages/python/dropbot-plugin/dropbot_plugin/__init__.py
+++ packages/python/dropbot-plugin/dropbot_plugin/__init__.py
@@ -1,16 +1,27 @@
 from micropede.client import MicropedeClient
 
+SCHEMAS = {
+    "voltage": {
+        "type": "integer"
+    }
+}
+
 class DropBot(MicropedeClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.voltage = 0
+        self.frequency = 0
+        self.schemas = SCHEMAS
 
     def listen(self):
         print("LISTENING!!", self.name)
         self.on_put_msg("voltage", self.put_voltage)
         self.on_put_msg("frequency", self.put_frequency)
+        self.on_put_msg("all", self.put_all)
 
     def put_voltage(self, payload, params):
         print("PUTTING VOLTAGE!")
+        self.voltage = payload["voltage"]
         try:
             return self.notify_sender(payload, "done", "voltage")
         except Exception as e:
@@ -18,10 +29,20 @@ class DropBot(MicropedeClient):
 
     def put_frequency(self, payload, params):
         print("PUTTING FREQUENCY")
+        self.frequency = payload["frequency"]
         try:
             return self.notify_sender(payload, "done", "frequency")
         except Exception as e:
             return self.notify_sender(payload, "done", "frequency", "failed")
+
+    def put_all(self, payload, params):
+        print("PUTTING ALL!")
+        self.voltage = payload["voltage"]
+        self.frequency = payload["frequency"]
+        try:
+            return self.notify_sender(payload, "done", "all")
+        except Exception as e:
+            return self.notify_sender(payload, "done", "all", "failed")
 
 print("Running dropbot plugin")
 dropbot = DropBot("microdrop", host="localhost", port=1884, name="dropbot")
