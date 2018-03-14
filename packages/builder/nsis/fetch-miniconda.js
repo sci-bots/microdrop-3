a91cd4abd729ca54cf7d4583bb3f9bb38959494e
lucaszw <lucas.zeer@gmail.com>
2018-03-14 09:42:06 -0400

    install miniconda silently

--- packages/builder/nsis/fetch-miniconda.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-
-const MINICONDA_URL = 'https://repo.continuum.io/miniconda/Miniconda2-latest-Windows-x86.exe';
-const file = fs.createWriteStream(path.resolve(__dirname, "miniconda.exe"));
-const request = https.get(MINICONDA_URL, function(response) {
-  response.pipe(file);
-});
--- packages/builder/nsis/init.js
+++ packages/builder/nsis/init.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
+const {spawnSync} = require('child_process');
 
 function moveMicrodropOutput() {
   const packagerFolder = path.resolve(__dirname, '../packager');
@@ -9,19 +10,23 @@ function moveMicrodropOutput() {
     if (path.extname(file) == '') {
       let oldPath = path.resolve(packagerFolder, file);
       let newPath = path.resolve(__dirname, 'MicroDrop');
-      fs.rename(oldPath, newPath, function (err) {
-        if (err) throw err
-      });
+      fs.renameSync(oldPath, newPath);
     }
   });
 }
 
 function fetchMiniconda() {
   const MINICONDA_URL = 'https://repo.continuum.io/miniconda/Miniconda2-latest-Windows-x86.exe';
-  const file = fs.createWriteStream(path.resolve(__dirname, "miniconda.exe"));
+  const filepath = path.resolve(__dirname, "miniconda.exe");
+  const file = fs.createWriteStream(filepath);
   const request = https.get(MINICONDA_URL, function(response) {
     response.pipe(file);
   });
+
+  file.on('finish', function(){
+    spawnSync(`cmd /C start /wait "" ${filepath} /NoRegistry=1 /RegisterPython=0 /AddToPath=0 /InstallationType=JustMe /S /D=${path.resolve(__dirname, 'MicroDrop/miniconda')}`, [], {shell: true, stdio: 'inherit'});
+  });
+
 };
 
 moveMicrodropOutput();
