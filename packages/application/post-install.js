ad3f0594b6df0179d30ebcf8b6235ab33afceb57
lucaszw <lucas.zeer@gmail.com>
2018-03-27 12:28:03 -0400

    fix missing node-cli.js issue on windows

--- packages/application/post-install.js
+++
@@ -1,9 +0,0 @@
-const hasbin = require('hasbin');
-
-result = hasbin.sync('conda');
-
-if (result == false)
-  console.error(`
-    Microdrop requires conda to manage process plugins.
-    Please install conda and add to your path.
-  `);
